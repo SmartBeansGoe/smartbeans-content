f00d19a5a3f8ca8f1d9dbe248684c02b9133a668
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-13 18:28:03 +0100

    Add asset output generator

--- src/generate-output.py
+++ src/generate-output.py
@@ -3,27 +3,38 @@ import os
 
 PATH = "material/outfits"
 
+
 def load_data_from_json(path):
     if not os.path.exists(f"{path}/meta.json"):
         print(f"ERR: Meta JSON file not found for \"{path}\"")
         return []
     with open(f"{path}/meta.json", "r") as fp:
         return json.load(fp)
-
-def filter_for_empty_svg(meta):
-    return [asset for asset in meta if asset["svg"] != ""]
         
+def deleteSVGEntry(loadedJSONItem):
+    svg = loadedJSONItem.pop('svg', None)
+    with open(f"out/assets/{loadedJSONItem['svgPath']}", "w") as file:
+        file.write(svg)
+
+    return loadedJSONItem
+
 
 if __name__ == '__main__':
     output = []
     outfits = [name for name in os.listdir(
         PATH) if os.path.isdir(os.path.join(PATH, name))]
     for outfit in outfits:
-        output += load_data_from_json(f"{PATH}/{outfit}")
-    output = filter_for_empty_svg(output)
+        loaded = load_data_from_json(f"{PATH}/{outfit}")
+        loaded = filter(lambda x: x["svg"] != "", loaded)
+
+        loaded = map(lambda x: deleteSVGEntry(x), loaded)
+        
+        output += loaded
+        
+    
     ids = [asset["id"] for asset in output]
     print(ids)
 
 
-    with open('out/assets-frontend.json', 'w') as fp:
+    with open('out/assets-backend.json', 'w') as fp:
         json.dump(output, fp)
